4c2dc6c688b576f599709549a5952cfe772e6971
Diogo Miguel Oliveira de Sousa <up201804265@fe.up.pt>
2022-05-13 17:36:28 +0000

    Update JmmOptimizer.java

--- src/pt/up/fe/comp/Ollir/JmmOptimizer.java
+++ src/pt/up/fe/comp/Ollir/JmmOptimizer.java
@@ -1,5 +1,7 @@
 package pt.up.fe.comp.Ollir;
 
+import java.util.Collections;
+
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
 import pt.up.fe.comp.jmm.ollir.JmmOptimization;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
@@ -8,8 +10,24 @@ public class JmmOptimizer implements JmmOptimization{
 
     @Override
     public OllirResult toOllir(JmmSemanticsResult semanticsResult) {
-        // TODO Auto-generated method stub
-        return null;
+        var OllirGenerator = new OllirGenerator(semanticsResult.getSymbolTable());
+        OllirGenerator.visit(semanticsResult.getRootNode());
+        var ollirCode = OllirGenerator.getCode();
+
+        System.out.println("OLLIR CODE : \n" + ollirCode);
+
+
+/*        String ollirCode = "import ioPlus;\r\n"
+        + "import BoardBase;\r\n"
+        + "import java.io.File;\r\n"
+        + "\r\n"        
+        + "class HelloWorld  extends BoardBase{\r\n"
+        + "\r\n"
+        + "\r\n"
+        +"}";
+*/
+        return new OllirResult(semanticsResult, ollirCode, Collections.emptyList());
     }
+
     
 }
