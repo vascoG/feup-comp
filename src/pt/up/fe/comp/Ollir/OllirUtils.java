0729e23a1ae0f3aea9f123ac1cd4c039a6caf44d
Catarina Raquel Silva GonASSalves <up201906638@fe.up.pt>
2022-05-16 12:23:54 +0000

    Update getOperation, TypeExpress and IdentifierExpress

--- src/pt/up/fe/comp/Ollir/OllirUtils.java
+++ src/pt/up/fe/comp/Ollir/OllirUtils.java
@@ -32,9 +32,48 @@ public class OllirUtils {
         }
     }
 
+    public static String getOperation(JmmNode node){
+        switch(node.getKind()){
+            case "And": return "&&.bool";
+            case "Not": return "!.bool";
+            case "Less": return "<.i32";
+            case "Add": return "+.i32";
+            case "Sub": return "-.i32";
+            case "Mult": return "*.i32";
+            case "Div": return "/.i32";
+            default: return "";
+        }
+    }
+
     //funçao para devolver o tipo através da expressao
-    public static String TypeExpress(JmmNode exprStmt){
-        return "0";
+    public static String TypeExpress(String exprStmt){
+        String aux = "";
+        int index = exprStmt.lastIndexOf('.');
+
+        if(exprStmt.equals("")) return "";
+
+        if(exprStmt.length() > 6 && index > 5f){
+            if(exprStmt.startsWith("array", index-5)) aux += ".array";
+        }
+
+        return aux + "." + exprStmt.substring(index+1).trim().replaceAll("[() ;+<*/&\\-]", "");
+    }
+
+    public static String IdentifierExpress(String exprStmt) { 
+        String[] values = exprStmt.split("\\.");
+
+        if(values.length == 2) return values[0].trim();
+
+        if(values.length > 2){
+            int index = 1;
+            String removelast = values[values.length-2];
+
+            if(removelast.equals("array")) index = 2;
+
+            List<String> aux = new ArrayList<>(Arrays.asList(values).subList(0, values.length-index));
+            return String.join(".", aux);
+        }
+        return exprStmt;
     }
 }
 
